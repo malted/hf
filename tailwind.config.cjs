3b6dacdafa8b14c5df483f6f0cdc903e7b27cbd8
Malted <malted@malted.dev>
2023-11-03 16:39:44 -0400

    ✨ More cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,13 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
+				transparent: "transparent",
+				black: "#222",
+				"dark-green": "#194b30",
+				"cta-green": "#4de291",
+				"text-green": "#2ab469",
 				muted: "#F1F1F1",
+				"muted-medium": "E5E5E5",
 				"muted-darker": "#767676"
 			},
 			fontFamily: {
