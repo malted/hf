fce61f17fe87a7b208b381f0c947b7cad89f8719
Malted <malted@malted.dev>
2023-11-18 15:39:14 -0500

    🐛 Fix background bug

---
+++ src/components/Card.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let id, classes = "", style;
+
+	//#region Padding class
+	const defaultPaddingClass = "p-5";
+	const defaultGapClass = "gap-4";
+	let c = classes.split(" ");
+	if (!c.find((c) => c.startsWith("p-"))) c.push(defaultPaddingClass);
+	if (!c.find((c) => c.startsWith("gap-"))) c.push(defaultGapClass);
+	classes = c.join(" ");
+	//#endregion
+</script>
+
+<div
+	{id}
+	class={`
+relative
+flex col gap-4 items-start justify-between
+h-fit w-full 
+text-muted-darker bg-white
+rounded-lg 
+border border-muted-medium
+overflow-hidden
+${classes}
+`}
+	{style}
+>
+	<slot />
+</div>
---
+++ src/components/showcase/components/Rotator.svelte
@@ -0,0 +1,78 @@
+<script>
+	import { onMount } from "svelte";
+	import anime from "animejs";
+
+	export let id;
+	let slotContainer;
+
+	onMount(() => {
+		const parentElement = slotContainer.parentElement.parentElement;
+		const duration = 2_000;
+		const childCount = slotContainer.children[0].children.length;
+		const restingElementStyle = "transform: translateY(-100%); opacity: 0; rotateX: 90deg;";
+		const elementSelectors = [], animators = [], intervals = [], timeouts = [];
+		function registerAnimations() {
+			for (let idx = 0; idx < childCount; idx++) {
+				const elementSelector = `#${id} #${id}-${idx + 1}`;
+				elementSelectors.push(elementSelector);
+				timeouts.push(
+					setTimeout(() => {
+						const self = anime
+							.timeline({ duration, easing: "easeInOutExpo" })
+							.add({
+								targets: elementSelector,
+								translateY: ["-100%", 0],
+								rotateX: ["90deg", 0],
+								scale: [0.8, 1],
+								opacity: [0, 1]
+							})
+							.add({
+								targets: elementSelector,
+								translateY: [0, "100%"],
+								rotateX: [0, "-90deg"],
+								scale: [1, 0.8],
+								opacity: [1, 0]
+							});
+						animators.push(self);
+						intervals.push(setInterval(() => self.restart(), childCount * duration));
+					}, idx * duration)
+				);
+				// It took me many moons to get this system down
+			}
+		}
+
+		function registerOrUnregisterAnimations(visible) {
+			if (visible) {
+				registerAnimations();
+			} else {
+				animators.forEach((animator) => animator.pause());
+				intervals.forEach((interval) => clearInterval(interval));
+				timeouts.forEach((timeout) => clearTimeout(timeout));
+				elementSelectors.forEach((selector) => document.querySelector(selector).style = restingElementStyle);
+				animators.length = intervals.length = timeouts.length = elementSelectors.length = 0;
+			}
+		}
+
+		new IntersectionObserver(
+			(entries) => registerOrUnregisterAnimations(entries[0].isIntersecting),
+			{
+				root: document.body,
+				rootMargin: "0px",
+				threshold: 1.0
+			}
+		).observe(parentElement);
+		document.addEventListener("visibilitychange", () =>
+			registerOrUnregisterAnimations(document.visibilityState == "visible")
+		);
+	});
+</script>
+
+		
+<div
+	{id}
+	class="relative w-full h-12 text-left"
+	style="perspective: 200px; transform-origin: center;"
+	bind:this={slotContainer}
+>
+	<slot />
+</div>
