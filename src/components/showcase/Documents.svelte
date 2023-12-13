55bf545b3d9addeb2d0881d8058a729246f47040
Malted <malted@malted.dev>
2023-12-13 19:49:04 +0000

    foo

--- src/components/showcase/Documents.svelte
+++ src/components/showcase/Documents.svelte
@@ -7,97 +7,71 @@
 	// const speed = "slow";
 
 	const duration = 4_500;
+	const docCount = 4;
+	const durationPer = duration / docCount;
 
+	const translateX = [
+		{ value: "3rem", duration: durationPer, delay: 150 },	
+		{ value: "6rem", duration: durationPer, delay: 100 },
+		{ value: "9rem", duration: durationPer, delay: 50 },
+		{ value: "12rem", duration: durationPer, delay: 50 }, 
+	];
+	const opacity = [
+		{ value: 1, duration: durationPer },
+		{ value: 1, duration: durationPer * (docCount - 2)},
+		{ value: 0, duration: 900 }
+	];
+	const update = (anim) => {
+		anim.animatables[0].target.style.zIndex = anim.animations[0].currentValue[0];
+	};
+	const animeConfig = {
+		translateX,
+		opacity,
+		duration,
+		easing: "easeInOutExpo",
+		loop: true,
+		update,
+	};
+	
 	onMount(() => {
-		anime({
-			targets: "#documents .placeholder-line",
-			width: (_, i) => [0, i === 2 ? "40%" : "100%"],
-			duration: 800,
-			easing: "easeInOutExpo",
-			delay: (el, i) => (150 * i) + 800,
-		});
+		// anime({
+		// 	targets: "#documents .placeholder-line",
+		// 	width: (_, i) => [0, i === 2 ? "40%" : "100%"],
+		// 	duration: 800,
+		// 	easing: "easeInOutExpo",
+		// 	delay: (el, i) => (150 * i) + 800,
+		// });
 
-		anime({
-			targets: "#documents .forms img#form-1",
-			translateX: [
-				{ value: "3rem", duration: duration / 3 },	
-				{ value: "6rem", duration: duration / 3 },
-				{ value: "9rem", duration: duration / 3 },
-			],
-			  opacity: [
-				{ value: 1, duration: duration / 3 },
-				{ value: 1, duration: duration / 3 },
-				{ value: 0, duration: duration / 3 }
-			],
-			duration,
-			easing: "easeInOutExpo",
-			loop: true,
-			update: (anim) => {
-				anim.animatables[0].target.style.zIndex = anim.animations[0].currentValue[0];
-			}
-		});
+		anime({ targets: "#documents .forms img#form-1", ...animeConfig });
 
 		setTimeout(() => {
-			const targets = "#documents .forms img#form-2";
-			
-			anime({
-				targets,
-				translateX: [
-					{ value: "3rem", duration: duration / 3 },	
-					{ value: "6rem", duration: duration / 3 },
-					{ value: "9rem", duration: duration / 3 },
-				],
-				opacity: [
-					{ value: 1, duration: duration / 3 },
-					{ value: 1, duration: duration / 3 }, // Maintain opacity for first 2s
-					{ value: 0, duration:duration / 3 }   // Then fade to transparent
-				],
-				duration,
-				easing: "easeInOutExpo",
-				loop: true,
-				update: (anim) => {
-					anim.animatables[0].target.style.zIndex = anim.animations[0].currentValue[0];
-				}
-			});
-		}, duration / 3 + 100);
+			anime({ targets: "#documents .forms img#form-2", ...animeConfig });
+		}, durationPer);
 
 		setTimeout(() => {
-			anime({
-				targets: "#documents .forms img#form-3",
-				translateX: [
-					{ value: "3rem", duration: duration / 3 },	
-					{ value: "6rem", duration: duration / 3 },
-					{ value: "9rem", duration: duration / 3 },
-				],
-				opacity: [
-					{ value: 1, duration: duration / 3 },
-					{ value: 1, duration: duration / 3 }, // Maintain opacity for first 2s
-					{ value: 0, duration: duration / 3 }   // Then fade to transparent
-				],
-				duration,
-				easing: "easeInOutExpo",
-				loop: true,
-				update: (anim) => {
-					anim.animatables[0].target.style.zIndex = anim.animations[0].currentValue[0];
-				}
-			});
-		}, duration / 3 * 2 + 200);
+			anime({ targets: "#documents .forms img#form-3", ...animeConfig });
+		}, durationPer * 2);
+
+		setTimeout(() => {
+			anime({ targets: "#documents .forms img#form-4", ...animeConfig });
+		}, durationPer * 3);
 	});
 </script>
 
 <Card id="documents" classes="justify-start grow-0">
 	<p class="text-xl font-medium">Legal documents</p>
 
-	<div class="flex col gap-2 w-full">
+	<!-- <div class="flex col gap-2 w-full">
 		<div class="w-0 h-1 rounded bg-muted placeholder-line"></div>
 		<div class="w-0 h-1 rounded bg-muted placeholder-line"></div>
 		<div class="w-0 h-1 rounded bg-muted placeholder-line"></div>
-	</div>
+	</div> -->
 
-	<div class="forms w-full h-28 flex-grow-0 overflow-hidden relative">
+	<div class="forms w-full h-24 flex-grow-0 overflow-hidden relative">
 		<img id="form-1" src="/w9.png" alt="IRS W9 tax form" class="-left-8 absolute opacity-0 rounded-lg border border-muted-medium" style="tranform: translateX(-2rem);" />
 		<img id="form-2" src="/cert.png" alt="Nonprofit certificate of status" class="-left-8 absolute opacity-0 rounded-lg border border-muted-medium" />
 		<img id="form-3" src="/irsletter.png" alt="IRS letter" class="-left-8 absolute opacity-0 rounded-lg border border-muted-medium" />
+		<img id="form-4" src="/hcletter.png" alt="Hack Club donor receipt" class="-left-8 absolute opacity-0 rounded-lg border border-muted-medium" />
 	</div>
 
 	<!-- <div class="h-16 overflow-hidden flex-grow-0 relative border border-red-500 w-full h-full"> -->
