f05851df4fdcc48587a5af41c95dc79e9e63a7e4
Malted <malted@malted.dev>
2023-12-13 19:15:17 +0000

    💫 Documents animating

--- src/components/showcase/Documents.svelte
+++ src/components/showcase/Documents.svelte
@@ -2,9 +2,11 @@
 	import { onMount } from "svelte";
 	import anime from "animejs";
 	import Card from "../Card.svelte";
-	import Marquee from "svelte-marquee";
 
-	const speed = "slow";
+	// import Marquee from "svelte-marquee";
+	// const speed = "slow";
+
+	const duration = 2_000;
 
 	onMount(() => {
 		anime({
@@ -12,8 +14,74 @@
 			width: (_, i) => [0, i === 2 ? "40%" : "100%"],
 			duration: 800,
 			easing: "easeInOutExpo",
-			delay: (el, i) => (150 * i) + 800
+			delay: (el, i) => (150 * i) + 800,
 		});
+
+		anime({
+			targets: "#documents .forms img#form-1",
+			translateX: [
+				{ value: "3rem", duration: 1_000 },	
+				{ value: "6rem", duration: 1_000 },
+				{ value: "9rem", duration: 1_000 },
+			],
+			  opacity: [
+				{ value: 1, duration: 1_000 },
+				{ value: 1, duration: 1_000 }, // Maintain opacity for first 2s
+				{ value: 0, duration: 1000 }   // Then fade to transparent
+			],
+			duration,
+			easing: "easeInOutExpo",
+			loop: true,
+			update: (anim) => {
+				anim.animatables[0].target.style.zIndex = anim.animations[0].currentValue[0];
+			}
+		});
+
+		setTimeout(() => {
+			const targets = "#documents .forms img#form-2";
+			
+			anime({
+				targets,
+				translateX: [
+					{ value: "3rem", duration: 1_000 },	
+					{ value: "6rem", duration: 1_000 },
+					{ value: "9rem", duration: 1_000 },
+				],
+				opacity: [
+					{ value: 1, duration: 1_000 },
+					{ value: 1, duration: 1_000 }, // Maintain opacity for first 2s
+					{ value: 0, duration: 1000 }   // Then fade to transparent
+				],
+				duration,
+				easing: "easeInOutExpo",
+				loop: true,
+				update: (anim) => {
+					anim.animatables[0].target.style.zIndex = anim.animations[0].currentValue[0];
+				}
+			});
+		}, 1_100);
+
+		setTimeout(() => {
+			anime({
+				targets: "#documents .forms img#form-3",
+				translateX: [
+					{ value: "3rem", duration: 1_000 },	
+					{ value: "6rem", duration: 1_000 },
+					{ value: "9rem", duration: 1_000 },
+				],
+				opacity: [
+					{ value: 1, duration: 1_000 },
+					{ value: 1, duration: 1_000 }, // Maintain opacity for first 2s
+					{ value: 0, duration: 1000 }   // Then fade to transparent
+				],
+				duration,
+				easing: "easeInOutExpo",
+				loop: true,
+				update: (anim) => {
+					anim.animatables[0].target.style.zIndex = anim.animations[0].currentValue[0];
+				}
+			});
+		}, 2_100);
 	});
 </script>
 
@@ -26,7 +94,16 @@
 		<div class="w-0 h-1 rounded bg-muted placeholder-line"></div>
 	</div>
 
-	<div class="marquees w-32">
+	<div class="forms w-full h-32 flex-grow-0 border border-red-500 overflow-hidden relative">
+		<img id="form-1" src="/w9.png" alt="IRS W9 tax form" class="-left-8 absolute opacity-0 rounded-lg border border-muted-medium" style="tranform: translateX(-2rem);" />
+		<img id="form-2" src="/cert.png" alt="Nonprofit certificate of status" class="-left-8 absolute opacity-0 rounded-lg border border-muted-medium" />
+		<img id="form-3" src="/irsletter.png" alt="IRS letter" class="-left-8 absolute opacity-0 rounded-lg border border-muted-medium" />
+	</div>
+
+	<!-- <div class="h-16 overflow-hidden flex-grow-0 relative border border-red-500 w-full h-full"> -->
+	
+
+	<!-- <div class="marquees w-32">
 		<Marquee
 			{speed}
 			content="Fiscal sponsorship confirmation | Reciepts"
@@ -40,11 +117,11 @@
 			speed="medium"
 			content="Certificate of status | Event liability forms"
 		/>
-	</div>
+	</div> -->
 </Card>
 
-<style>
+<!-- <style>
 	:global(#documents .marquees span) {
 		font-size: 0.75rem; line-height: 1rem;
 	}
-</style>
+</style> -->
