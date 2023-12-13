7bd6e82811523a88257e5ad001908dea641c933c
Malted <malted@malted.dev>
2023-12-13 01:59:26 +0000

    ✨ Make the final two cards look somewhat presentable

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: squooshImageService()
-  },
-  integrations: [tailwind(), svelte()]
+	image: {
+		service: squooshImageService()
+	},
+	integrations: [tailwind(), svelte()]
 });
---
+++ src/components/showcase/Documents.svelte
@@ -0,0 +1,50 @@
+<script>
+	import { onMount } from "svelte";
+	import anime from "animejs";
+	import Card from "../Card.svelte";
+	import Marquee from "svelte-marquee";
+
+	const speed = "slow";
+
+	onMount(() => {
+		anime({
+			targets: "#documents .placeholder-line",
+			width: (_, i) => [0, i === 2 ? "40%" : "100%"],
+			duration: 800,
+			easing: "easeInOutExpo",
+			delay: (el, i) => (150 * i) + 800
+		});
+	});
+</script>
+
+<Card id="documents" classes="justify-start grow-0">
+	<p class="text-xl font-medium">Legal documents</p>
+
+	<div class="flex col gap-2 w-full">
+		<div class="w-0 h-1 rounded bg-muted placeholder-line"></div>
+		<div class="w-0 h-1 rounded bg-muted placeholder-line"></div>
+		<div class="w-0 h-1 rounded bg-muted placeholder-line"></div>
+	</div>
+
+	<div class="marquees w-32">
+		<Marquee
+			{speed}
+			content="Fiscal sponsorship confirmation | Reciepts"
+		/>
+		<Marquee
+			{speed}
+			reverse={true}
+			content="Form W-9 | IRS 501(c)(3) Documentation Letter"
+		/>
+		<Marquee
+			speed="medium"
+			content="Certificate of status | Event liability forms"
+		/>
+	</div>
+</Card>
+
+<style>
+	:global(#documents .marquees span) {
+		font-size: 0.75rem; line-height: 1rem;
+	}
+</style>
